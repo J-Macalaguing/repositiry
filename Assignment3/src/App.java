d9de7a8dff9abf2f9e1ac622c50f4d09df3a99ad
J-Macalaguing <jedd.macalaguing@student.tdsb.on.ca>
2025-02-24 17:45:33 -0500

    App.java

--- Assignment3/src/App.java
+++ Assignment3/src/App.java
@@ -1,5 +1,14 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+public class AmbiguousCase {
+    public static void main(String[] args)  {
+    Scanner k = new Scanner(System.in);
+    System.out.println("Give me an angle");
+    double angleA = nextDouble();
+    System.out.println("Give me side value A");
+    double sideA = nextDouble();
+    System.out.println("Give me side value B");
+    double sideB = nextDouble();
+    
     }
 }
+
