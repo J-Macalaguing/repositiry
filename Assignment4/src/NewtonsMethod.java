0a7538eb3a0e631f20e91a9d36df893f7967390e
J-Macalaguing <jedd.macalaguing@student.tdsb.on.ca>
2025-04-03 13:19:18 -0400

    1

--- Assignment4/src/NewtonsMethod.java
+++ Assignment4/src/NewtonsMethod.java
@@ -1,14 +1,31 @@
 import java.util.Scanner;
+
 public class NewtonsMethod {
     public static void main(String[] args) {
-    Scanner kb = new Scanner(System.in);
-    System.out.println("initial root guess");
-    double x = kb.nextDouble();
-    
-    double func = 6 * Math.pow(x, 4) - 13 * Math.pow(x, 3) - 18 * Math.pow(x, 2) + 7 * x + 6;
-    double deriv = 24 * Math.pow(x, 3) - 39 * Math.pow(x, 2) - 36 * x + 7;
-    
-    double oijnfe = x - func / deriv;
-    
+        Scanner kb = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("initial root guess");
+            double x2 = kb.nextDouble();
+
+            while (true) {
+                double func = 6 * Math.pow(x2, 4) - 13 * Math.pow(x2, 3) - 18 * Math.pow(x2, 2) + 7 * x2 + 6;
+                double deriv = 24 * Math.pow(x2, 3) - 39 * Math.pow(x2, 2) - 36 * x2 + 7;
+                double x1 = x2 - func / deriv;
+
+                if (Math.abs(x1 - x2) < 0.00000000001) {
+                    System.out.println("Root approximation: " + x1);
+                    break;
+                }
+
+                x2 = x1;
+            }
+            System.out.println("try again?");
+            if (kb.next().charAt(0) == 'n') {
+                kb.close();
+                break;
+            }
+        }
+
     }
 }
--- Assignment5/src/PolynomialEvaluator.java
+++ Assignment5/src/PolynomialEvaluator.java
@@ -1,12 +1,20 @@
+import java.util.StringTokenizer;
 import java.util.Scanner;
+
 public class PolynomialEvaluator {
     public static void main(String[] args) {
-       Scanner kb = new Scanner(System.in);
-       System.out.println("give polynomial with 5 terms pls");
-       String poly = kb.nextLine();
-       System.out.println("Give me an x value");
-       String x = kb.nextLine();
+        Scanner kb = new Scanner(System.in);
+        System.out.println("give polynomial with 5 terms pls");
+        String poly = kb.nextLine();
+        System.out.println("Give me an x value");
+        double x = kb.nextDouble();
 
-       jfjhfdehgfs
+        StringTokenizer polyToken = new StringTokenizer(poly, " ");
+        while (polyToken.hasMoreTokens()) {
+            System.out.println(polyToken.nextToken());
+        }
+
+        
     }
 }
+// weiner
